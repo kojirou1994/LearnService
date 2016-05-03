2bcfa356eec546ded935036ee40fb43de65d865a
kojirou <kojirouhtc@gmail.com>
2016-05-03 20:15:54 +0800

    finished

--- app/src/main/java/com/putotyra/learnservice/EchoService.java
+++ app/src/main/java/com/putotyra/learnservice/EchoService.java
@@ -2,15 +2,74 @@ package com.putotyra.learnservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class EchoService extends Service {
+
+    private Timer timer = null;
+    private TimerTask task = null;
+
+    private int i = 0;
+
+    public void startTimer() {
+        if (timer == null) {
+            timer = new Timer();
+            task = new TimerTask() {
+                @Override
+                public void run() {
+                    i++;
+                    System.out.println(i);
+                }
+            };
+            timer.schedule(task, 1000, 1000);
+        }
+    }
+
+    public void stopTimer() {
+        if (timer != null) {
+            task.cancel();
+            timer.cancel();
+
+            task = null;
+            timer = null;
+        }
+    }
+
+    public int getCurrentNum() {
+        return i;
+    }
     public EchoService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        System.out.println("onBind");
+        return echoServiceBinder;
+    }
+
+    private final EchoServiceBinder echoServiceBinder = new EchoServiceBinder();
+
+    public class EchoServiceBinder extends Binder {
+        public EchoService getService() {
+            return EchoService.this;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        System.out.println("onCreate");
+        startTimer();
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        System.out.println("onDestroy");
+        stopTimer();
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/putotyra/learnservice/MainActivity.java
+++ app/src/main/java/com/putotyra/learnservice/MainActivity.java
@@ -1,13 +1,78 @@
 package com.putotyra.learnservice;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ServiceConnection {
+
+    private Button btnStartService, btnStopService, btnBindService, btnUnbindService, btnGetCurrentNumber;
+    private Intent serviceIntent;
+    private EchoService echoService = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        serviceIntent = new Intent(this, EchoService.class);
+
+        btnStartService = (Button) findViewById(R.id.btnStartService);
+        btnStopService = (Button) findViewById(R.id.btnStopService);
+        btnBindService = (Button) findViewById(R.id.btnBindService);
+        btnUnbindService = (Button) findViewById(R.id.btnUnbindService);
+        btnGetCurrentNumber = (Button) findViewById(R.id.btnGetCurrentNumber);
+
+        btnStartService.setOnClickListener(this);
+        btnStopService.setOnClickListener(this);
+        btnBindService.setOnClickListener(this);
+        btnUnbindService.setOnClickListener(this);
+        btnGetCurrentNumber.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStartService:
+                startService(serviceIntent);
+                break;
+            case R.id.btnStopService:
+                stopService(serviceIntent);
+                break;
+            case R.id.btnBindService:
+                bindService(serviceIntent, this, Context.BIND_AUTO_CREATE);
+                break;
+            case R.id.btnUnbindService:
+                if (echoService != null) {
+                    unbindService(this);
+                    echoService = null;
+                }
+                break;
+            case R.id.btnGetCurrentNumber:
+                if (echoService != null) {
+                    System.out.println("当前服务中的数字是：" + echoService.getCurrentNum());
+                }
+                break;
+//            default:
+//                break;
+        }
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        System.out.println("onServiceConnected");
+
+        echoService = ((EchoService.EchoServiceBinder) service).getService();
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        System.out.println("onServiceDisconnected");
     }
 }
